768d6a273b8f4ea17b568859463b2f21fbb64e89
shally <shallysubhash@gmail.com>
2019-05-11 21:02:51 +0530

    Add files via upload

---
+++ Insertdata.py
@@ -0,0 +1,78 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+
+from database_setup import Category, Item, Base
+
+engine = create_engine('sqlite:///itemcatalog.db')
+
+# Clear database
+Base.metadata.drop_all(engine)
+Base.metadata.create_all(engine)
+
+Base.metadata.bind = engine
+
+DBSession = sessionmaker(bind=engine)
+
+session = DBSession()
+
+
+
+# Books for Tolstoy
+category1 = Category(name="Tolstoy")
+
+session.add(category1)
+session.commit()
+
+item1 = Item(name="War and Peace",  description="year_written: 1865,", category=category1)
+
+session.add(item1)
+session.commit()
+
+item2 = Item(name="Anna Karenina",   description="year_written: 1875", category=category1)
+
+session.add(item2)
+session.commit()
+
+item3 = Item(name="The Death of Ivan Ilyich", description="Another of Tolstoy’s celebrated novellas, The Death of Ivan Ilyich treats the sensitive theme of death and dying", category=category1)
+
+session.add(item3)
+session.commit()
+
+item3 = Item(name="The Cossacks", description="Originally entitled ‘Young Manhood’, this short novel follows nobleman Dmitri Olenin who", category=category1)
+
+session.add(item3)
+session.commit()
+
+item3 = Item(name="The Kingdom of God Is Within You", description="Tolstoy’s 1894 philosophical treatise", category=category1)
+
+session.add(item3)
+session.commit()
+
+
+# books for Woolf, Virginia
+category2 = Category(name="Woolf, Virginia")
+
+session.add(category2)
+session.commit()
+
+item1 = Item(name="Mrs. Dalloway",  description="Mrs. Dalloway is one of the best books to start with for those who are only just encountering Virginia Woolf’s writing", category=category2)
+
+session.add(item1)
+session.commit()
+
+item2 = Item(name="Orlando: A Biography",   description="Orlando is an enthralling yet accessible read. It starts with a male protagonist, an aristocratic poet who frequents Queen Elizabeth’s court.", category=category2)
+
+session.add(item2)
+session.commit()
+
+item3 = Item(name="To the Lighthouse", description="The story of three members of the Ramsay family, told from their varying perspectives", category=category2)
+
+session.add(item3)
+session.commit()
+
+
+
+categories = session.query(Category).all()
+for category in categories:
+    print ("Category: " + category.name)
---
+++ database_setup.py
@@ -0,0 +1,51 @@
+import os
+import sys
+from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
+from sqlalchemy import create_engine
+ 
+Base = declarative_base()
+
+ 
+class Category(Base):
+    __tablename__ = 'Category'
+   
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250), nullable=False)
+
+    @property
+    def serialize(self):
+       
+           return {
+               'id'    : self.id,
+               'name'  : self.name
+           }
+ 
+ 
+class Item(Base):
+    __tablename__ = 'Item'
+
+
+    name =Column(String(80), nullable = False)
+    id = Column(Integer, primary_key = True)
+    description = Column(String(250))
+    Category_id = Column(Integer,ForeignKey('Category.id'))
+    category  = relationship(Category) 
+    
+
+#We added this serialize function to be able to send JSON objects in a serializable format
+    @property
+    def serialize(self):
+       
+       return {
+           'category'         : self.category.name,
+           'description'      : self.description,
+           'name'             : self.name
+       }
+ 
+
+engine = create_engine('sqlite:///itemcatalog.db')
+ 
+
+Base.metadata.create_all(engine)
